03d252418e53c2f32164edd8dcab0ea053c41568
Aaron Tan <hi@aaronhktan.com>
2019-12-14 23:43:06 +0000

    Add Python script that sends data to MQTT
    - This is the script that is running on the Onion to send the data to the Raspberry Pi.

---
+++ src/python/sender.py
@@ -0,0 +1,40 @@
+from collections import deque
+import serial
+
+import paho.mqtt.client as mqtt # For MQTT
+
+def on_connect(client, userdata, flags, rc):
+  print('Connected with result code {}'.format(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect('192.168.0.198', 1883, 60)
+
+serialPort = serial.Serial('/dev/ttyS1', 9600, timeout=2)
+if not serialPort.isOpen():
+  print("Error: Failed to initialize serial port")
+  exit()
+
+cache = deque()
+n = 60            # Max number of elements in deque
+t = 0             # Current number of elements in deque
+cum_sum = 0       # Cumulative sum
+avg = 0           # Running average over last n elements
+
+while True:
+  value = serialPort.readline()
+
+  cache.append(int(value))
+  cum_sum += int(value)
+  t += 1
+  if t <= n:
+    avg = int(cum_sum / float(t))
+  else:
+    cum_sum -= cache.popleft()
+    avg = int(cum_sum / float(n)) 
+  print(avg)
+
+  if not t % 60: # Only publish once every minute
+    client.publish('plants/soilmoisture', str(avg))
+ 
